8d5c677782afd1f39456285735de7e87fd513eea
lutfiads <152250212+LutfiAds@users.noreply.github.com>
2026-04-08 14:59:57 +0700

    chore: remove unused code

--- correlation/cross_host_detector.py
+++ correlation/cross_host_detector.py
@@ -5,7 +5,6 @@ def detect_cross_host_attack(df, host_threshold=2):
 
     df = df.copy()
 
-    # hitung jumlah hostname unik per IP
     host_counts = df.groupby("ip")["hostname"].nunique()
 
     suspicious_ips = host_counts[host_counts >= host_threshold].index
